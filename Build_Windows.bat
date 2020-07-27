55ec509559f397d42cf02a2c24d740452aec8d12
Tary <tary.hitwh@gmail.com>
2020-07-27 11:26:18 +0800

    fix 企业版 vs 及 资源路径

--- Build_Windows.bat
+++ Build_Windows.bat
@@ -6,6 +6,9 @@
 @REM PROTOBUF_UE4_PREFIX  - Absolute install path prefix string.
 @REM 
 
+@SET PROTOBUF_UE4_VERSION=3.6.1.3
+@SET PROTOBUF_UE4_PREFIX=H:\protobuf\build
+
 @if "%PROTOBUF_UE4_VERSION%"=="" (
     echo PROTOBUF_UE4_VERSION is not set, exit.
     exit /b 1
@@ -24,7 +27,7 @@ set PROTOBUF_CMAKE_OPTIONS=-Dprotobuf_MSVC_STATIC_RUNTIME=OFF
 
 @REM -----------------------------------------------------------------------
 @REM Set Environment Variables for the Visual Studio 2017 Command Line
-set VS2017DEVCMD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat
+set VS2017DEVCMD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat
 if exist "%VS2017DEVCMD%" (
     @REM Tell VsDevCmd.bat to set the current directory, in case [USERPROFILE]\source exists. See:
     @REM C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd\core\vsdevcmd_end.bat
@@ -35,17 +38,20 @@ if exist "%VS2017DEVCMD%" (
      exit /b 2
 )
 
-set PROTOBUF_URL=https://github.com/google/protobuf/releases/download/v%PROTOBUF_UE4_VERSION%/protobuf-cpp-%PROTOBUF_UE4_VERSION%.zip
+set PROTOBUF_URL=https://codeload.github.com/protocolbuffers/protobuf/zip/v%PROTOBUF_UE4_VERSION%
+rem https://github.com/google/protobuf/releases/download/v%PROTOBUF_UE4_VERSION%/protobuf-cpp-%PROTOBUF_UE4_VERSION%.zip
 set PROTOBUF_DIR=protobuf-%PROTOBUF_UE4_VERSION%
 set PROTOBUF_ZIP=protobuf-%PROTOBUF_UE4_VERSION%.zip
 
-rd /S /Q %PROTOBUF_DIR%
+rem rd /S /Q %PROTOBUF_DIR%
 
 REM Force Invoke-WebRequest to use TLS 1.2
-powershell -Command [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; ^
-    Invoke-WebRequest -Uri %PROTOBUF_URL% -OutFile %PROTOBUF_ZIP%
+rem // powershell -Command [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; ^
+rem //    Invoke-WebRequest -Uri %PROTOBUF_URL% -OutFile %PROTOBUF_ZIP%
 
-powershell -Command Expand-Archive -Path %PROTOBUF_ZIP% -DestinationPath .
+rem powershell -Command Expand-Archive -Path %PROTOBUF_ZIP% -DestinationPath .
+rem rd /S /Q %PROTOBUF_DIR%\third_party\googletest
+rem powershell -Command Expand-Archive -Path googletest.zip -DestinationPath %PROTOBUF_DIR%\third_party\
 
 set FIX_FILE=%cd%\Fix-%PROTOBUF_UE4_VERSION%.bat
 if exist "%FIX_FILE%" (
@@ -56,6 +62,18 @@ if exist "%FIX_FILE%" (
 
 rd %PROTOBUF_UE4_PREFIX%
 mkdir %PROTOBUF_UE4_PREFIX%
+
+
+
+
+
+cd %PROTOBUF_DIR%\cmake
+REM build client_android in MAC, error: undefined bswap_16 bswap_32 bswap_64
+powershell -Command "$text=Get-Content CMakeLists.txt ;" ^
+                    "$text[27]='option(protobuf_BUILD_TESTS "Build tests" OFF)' ;" ^
+                    "$text| Set-Content port.h"
+
+
 
 cd %CURRENT_DIR%
 cd %PROTOBUF_DIR%\cmake
@@ -68,14 +86,17 @@ pushd static
         -DCMAKE_BUILD_TYPE=Release ^
         -DCMAKE_INSTALL_PREFIX="%PROTOBUF_UE4_PREFIX%/static" ^
         %PROTOBUF_CMAKE_OPTIONS% ../..
+	cd static
     nmake
     nmake check
     nmake install
 popd
 
+pause
 mkdir %PROTOBUF_UE4_PREFIX%\windows\bin
 mkdir %PROTOBUF_UE4_PREFIX%\windows\lib
 
+pause
 move %PROTOBUF_UE4_PREFIX%\static\lib\libprotobuf.lib %PROTOBUF_UE4_PREFIX%\windows\lib\libprotobuf.lib
 move %PROTOBUF_UE4_PREFIX%\static\bin\protoc.exe %PROTOBUF_UE4_PREFIX%\windows\bin\protoc.exe
 move %PROTOBUF_UE4_PREFIX%\static\include %PROTOBUF_UE4_PREFIX%\windows\include
--- Fix-3.6.1.bat
+++ Fix-3.6.1.bat
@@ -4,7 +4,7 @@ cd %PROTOBUF_DIR%\src\google\protobuf
 
 REM see: https://medium.com/@0xflarion/using-ue4-w-google-protocol-buffers-ae7cab820d84 step2
 REM Error C4647	behavior change: __is_pod(google::protobuf::internal::AuxillaryParseTableField) has different value in previous versions \
-REM Pirates	C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\INCLUDE\type_traits
+REM Pirates	C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.14.26428\INCLUDE\type_traits
 powershell -Command "$text=Get-Content generated_message_table_driven.h ;" ^
                     "$text[172]='// ' ;" ^
                     "$text| Set-Content generated_message_table_driven.h"
